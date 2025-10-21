c151e52abbd5695c7a746e1daa1f925917df2138
Edson Manuel <94713991+Netchitchi@users.noreply.github.com>
2025-10-21 12:38:04 +0100

    update

--- packages/supabase-client/src/server-client.ts
+++ packages/supabase-client/src/server-client.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // ⚠ NUNCA expor no frontend!
+  process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY! // ⚠ NUNCA expor no frontend!
 );
